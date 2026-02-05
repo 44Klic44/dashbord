e5b589100b61c254f99fcbbccb2382ff81887ba5
ROMAN TYSACHNYY <stance_srt@mail.ru>
2026-02-05 20:58:53 +0300

    component Login

--- dashbord/src/pages/Login.jsx
+++ dashbord/src/pages/Login.jsx
@@ -11,7 +11,9 @@ const {
   } = useForm();
 
 const navigate = useNavigate();
-
+const submitHandler = async(data)=>{
+  console.log("submit")
+}
 useEffect(()=>{
 user && navigate("/Dashboard")
 
@@ -22,12 +24,12 @@ user && navigate("/Dashboard")
   return (
   <div className=' w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
     <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
-     
+    {/* left-block */}
      <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
           
           <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
             
-            <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base dark:border-gray-700 dark:text-blue-400 border-gray-300 text-gray-600'>
+            <span className='flex gap-5   py-8 px-8 border rounded-full text-sm md:text-base dark:border-gray-700 dark:text-blue-400 border-gray-300 text-gray-600'>
               Manage all your task in one place!
             </span>
 
@@ -44,8 +46,24 @@ user && navigate("/Dashboard")
 
         </div>
 
-
-
+{/* right-block */}
+<div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'>
+          <form
+            onSubmit={handleSubmit(submitHandler)}
+            className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white dark:bg-slate-900 px-10 pt-14 pb-14'
+          >
+            <div>
+              <p className='text-blue-600 text-3xl font-bold text-center'>
+                Welcome back!
+              </p>
+              <p className='text-center text-base text-gray-700 dark:text-gray-500'>
+                Keep all your credetials safe!
+              </p>
+            </div>
+            
+           
+          </form>
+        </div>
     </div>
     
   </div>
