a13f23efe1d8241cc4b46418881bbac8f4a73564
ROMAN TYSACHNYY <stance_srt@mail.ru>
2026-02-05 21:18:37 +0300

    Не работают отступы через стили tilwind

--- dashbord/vite.config.js
+++ dashbord/vite.config.js
@@ -2,19 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-
-  server :{
+  plugins: [react(), tailwindcss()],
+  server: {
     port: 3000,
     proxy: {
-      "/api":{
+      "/api": {
         target: "http://localhost:8800",
         changeOrigin: true,
       }
-
     }
   }
 })
